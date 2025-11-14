6a0677bda257b027e85cc19b768a4559b97e01d7
sarfrazalam2009-cpu <sarfrazalam2009@gmail.com>
2025-11-14 21:07:37 +0000

    datatype_use

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+console.log(3 + 3)
+
+console.log("Sarfraz")
+
+//var name = "sarfraz"
+let name = "Sarfraz"
+let age = 18
+let isloggedIn = false
+
+// number => 2 to  power 53
+// bigint
+// boolean => true/false
+// null => srandalone value
+//undefined => value not assign
+// symbol => unique
+// object
+console.log(typeof "Sarfraz"); // typeof is used to know data type 
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
