7d2881999741f12763669523173839f1f0a9bede
sarfrazalam2009-cpu <sarfrazalam2009@gmail.com>
2025-11-14 20:27:51 +0000

    variable_use

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sarfrazalam.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+//accountId = 2
+
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in blcok scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
