548589581b8f633b47eac75e1db86c4eefe8f33b
sarfrazalam2009-cpu <sarfrazalam2009@gmail.com>
2025-11-15 19:09:53 +0000

    conversion_datatype

---
+++ 01_basics/03_conversionoprations.js
@@ -0,0 +1,77 @@
+// Number datatype conversion Oprations 
+//let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// Number with alphabate string datatype conversion Oprations 
+ //let score = "33abc"
+ //console.log(typeof score);
+ //let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+
+ // Null datatype conversion oprations
+
+ //let score = null
+ //console.log(typeof score);
+ //let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+
+// what happen when value = undefine datatype
+
+//let score = undefined
+
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// what happen when value = boolean datatype
+
+//let score = false
+
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// all datatype conversion Oprations 
+
+//  "33" => 33
+//  "33abc" => NaN
+//  true => 1; false => 0
+
+
+//let isLoggedIn = 0
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0 => false
+// "" => false
+// "sarfraz" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+ 
+ 
+ 
+ 
+
+ 
+
