9d48b621f796d51b20afba90a3514e3df1deeb8c
sarfrazalam2009-cpu <sarfrazalam2009@gmail.com>
2025-11-15 19:37:32 +0000

    conversion_oprations

--- 01_basics/03_conversionoprations.js
+++ 01_basics/03_conversionoprations.js
@@ -63,8 +63,49 @@
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+//***************************************** Oprations ********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Sarfraz"
+// let str2 = " Alam"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+
+str1 = str2 = str3 = 2 + 2
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
 
 
 
