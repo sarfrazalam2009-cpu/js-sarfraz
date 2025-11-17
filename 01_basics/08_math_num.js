d79c716f5bd166dd6436ab1d2fbe9276ef5670ba
sarfrazalam2009-cpu <sarfrazalam2009@gmail.com>
2025-11-17 19:46:07 +0000

    maths

---
+++ 01_basics/08_math_num.js
@@ -0,0 +1,54 @@
+//*************************** Math **************************//
+
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());  // its convert foreign style 
+// console.log(hundreds.toLocaleString('en-IN')); // its convert indian value style
+
+
+
+// ******************************* Math ******************************* //
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // its change -ve value into +ve value
+
+// console.log(Math.round(4.66));
+// console.log(Math.ceil(3.2));   //its choose top value 
+// console.log(Math.floor(3.9));  //its choose bottom value 
+
+// console.log(Math.sqrt(4));
+// console.log(Math.min(4, 3, 5, 6));
+// console.log(Math.max(4, 6, 8, 9));
+
+console.log(Math.random());  // its give value b/w 0 or 1 so value will be 0.1,0.9
+console.log(Math.random()*10);  
+console.log(Math.random()*10+1);
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+
+const min = 10
+const max = 20
+Math.random() * (max - min + 1)
+console.log(Math.random() * (max - min + 1));
+console.log(Math.floor(Math.random() * (max - min + 1)));
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
