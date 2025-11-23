1863ebf1e7a543b763c5fb488054c57b01ae4232
sarfrazalam2009-cpu <sarfrazalam2009@gmail.com>
2025-11-23 20:09:16 +0000

    scope_hosting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,8 +2,6 @@
 // Note : in a scope value are block scope
 // outside of scope is globle scope
 
-const { forwardRef } = require("react");
-
 // if (true){
 //     let a = 10 // answer a is not define
 //     const b = 20 // answer b is not define 
@@ -25,6 +23,54 @@ const { forwardRef } = require("react");
 // answer is INNER: 10 -> that is Block Scope Because its is under the scope
 // answer is 300 -> that is Globel scope Because it is outside of the scope 
 
-// Nestet Scope
+// Nestet Scope -> in nestest scope child scope is take variable from its parents scope
+
+function one(){
+    const username = "sarfraz"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website); answer is Error
+    
+    two()
+}
+one()
+
+if(true){
+    const username = "Alam"
+
+    if(username === "Alam"){
+
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website); its show Error bcoz its out of scope
+    
+}
+// console.log(username);  its show Error bcoz its out of scope
+
+
+// ********************* interesting ****************************
+
+// one way of function declearation
+// when we consol using in top of function then program will return the value
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+
+}
+
+// another way of function declearation 
+// when we consol using in top of the function then program will return the Error bcoz we hold the value in a variable
+console.log(addTwo(5));
+
+const addTwo = function(num){
+    return num + 2
+}
 
 
