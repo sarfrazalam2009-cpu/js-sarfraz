7cba4d9a2cbf3c39e42e1f0faef9460d9a3fb09d
sarfrazalam2009-cpu <sarfrazalam2009@gmail.com>
2025-11-28 20:50:08 +0000

    forloop_study

---
+++ 05_itrations_loop/04_for_in_loop.js
@@ -0,0 +1,29 @@
+// for_in_loop its besicaly use in object 
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift bu apple'
+}
+for (const key in myObject) {
+//    console.log(myObject[key]);   
+   //console.log(`${key} shortcut is for ${myObject[key]}`);   
+}
+
+const programming = ["js", "rb", "cpp", "java", "py"]
+for (const key in programming) {
+// console.log(programming[key]);
+ 
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// //map.set('IN', "India")
+
+// for (const key in map) {
+//    console.log(key);
+   
+// }
