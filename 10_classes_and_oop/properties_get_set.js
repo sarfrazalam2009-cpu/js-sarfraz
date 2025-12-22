32093d0f9d582859a5a157b602d5a6070d481a4b
sarfrazalam2009-cpu <sarfrazalam2009@gmail.com>
2025-12-22 20:57:04 +0000

    functionbase_get_set_study

---
+++ 10_classes_and_oop/properties_get_set.js
@@ -0,0 +1,29 @@
+// old version of function base get and set
+
+function User(email, password){
+    this._email = email
+    this.password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+
+        }
+    })
+}
+
+const chai = new User("chai@chai.com", "chai")
+console.log(chai.email);
+
