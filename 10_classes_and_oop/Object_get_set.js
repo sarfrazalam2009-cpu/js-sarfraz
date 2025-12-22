a0fff51dc1d59816be9a709d0e05a6b0fed481a5
sarfrazalam2009-cpu <sarfrazalam2009@gmail.com>
2025-12-22 20:56:23 +0000

    Objectbase_get_set_study

---
+++ 10_classes_and_oop/Object_get_set.js
@@ -0,0 +1,18 @@
+// object base get and set
+
+const User = {
+    _email: 's@hv.com',
+    _password: "abc",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value 
+    }
+
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
